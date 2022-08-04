ca80710b6df25058e86f562aaba4471f38a2a6a6
Evaristo Alves <evaristocarvalho34@gmail.com>
2022-08-04 19:59:56 -0300

    primeiro commit

---
+++ MarketPlaceOnline/MarketPlaceOnline/ConnectionFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlaceOnline
+{
+    public class ConnectionFactory
+    {
+        public static IOrganizationService GetService()
+        {
+            string connectionString =
+                "AuthType= OAuth;" +
+                "Username=EvaristoAlves@TreinamentoDynamics2022.onmicrosoft.com;" +
+                "Password=P@ssword#;" +
+                "Url=https://orgc7aca8a9.crm2.dynamics.com/ ;" +
+                "AppId=ae54efe1-1540-456e-9fbc-ca228ce68145;" +
+                "RedirectUri= app://58145B91-0C36-4500-8554-080854F2AC97";
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            return crmServiceClient.OrganizationWebProxyClient;
+        }
+    }
+}
---
+++ MarketPlaceOnline/MarketPlaceOnline/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlaceOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            IOrganizationService service = ConnectionFactory.GetService();
+
+           
+            Entity conta = new Entity("account");
+            Console.WriteLine("Por favor, informe o nome da conta!");
+            conta["name"] = Console.ReadLine();
+
+            Console.WriteLine("Por favor, informe o site da conta!");
+            conta["websiteurl"] = Console.ReadLine(); 
+
+            Console.WriteLine("Por favor, informe o total de oportunidade!");
+            var valor= Convert.ToDecimal(Console.ReadLine());
+            conta["eac_totaldeoportunidades"] = new Money(valor);
+
+            Console.WriteLine("Por favor, informe o tamanho da empresa!");
+            var tamanho= Console.ReadLine();
+            if (tamanho=="pequena")
+            {
+                conta["eac_portedaempresa"] = new OptionSetValue(783910000);
+            }else if (tamanho == "média") 
+            {
+                conta["eac_portedaempresa"] = new OptionSetValue(783910001);
+            }
+            else
+            {
+                conta["eac_portedaempresa"] = new OptionSetValue(783910002);
+            }
+            
+            conta["eac_totaldeproduto"] = new Decimal(10.1);
+            Guid accountId = service.Create(conta);
+
+            Console.WriteLine("Você deseja criar um contato para esta conta? (S/N)");
+            var resposta = Console.ReadLine();
+
+            switch (resposta)
+            {
+                case"S":
+                    Entity contato = new Entity("contact");
+                    Console.WriteLine("Digite o primeiro nome do contato!");
+                    contato["firstname"] = Console.ReadLine();
+                    Console.WriteLine("Digite o primeiro último nome do contato!");
+                    contato["larstname"] = Console.ReadLine();
+                    Console.WriteLine("Digite o e-mail do contato!");
+                    contato["emailaddress1"] = Console.ReadLine();
+                    Console.WriteLine("Digite o celular do contato!");
+                    contato["mobilephone"] = Console.ReadLine();
+                    contato["parentcustomerid"] = new EntityReference("account", accountId);
+                    service.Create(contato);
+                    break;
+                case "N":
+                    Console.WriteLine("Fim do cadastro!");
+                    break;
+                    default:
+                    Console.WriteLine("Digite S para sim ou N para não");
+                    break;
+            }
+            
+            
+           
+            
+            
+
+        }
+    }
+}
